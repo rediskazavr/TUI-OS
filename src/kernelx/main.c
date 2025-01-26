303e60e49f074a6f6bedf4e226cf5a97de68f382
MatveySDK <matveywindows60@gmail.com>
2025-01-26 18:11:57 +0300

    Create kernelx with commands: clear, exit

---
+++ src/kernelx/main.c
@@ -0,0 +1,29 @@
+// main.c
+#include <stdio.h>
+#include <string.h>
+#include "exit.h"
+#include "clear.h"
+
+void execute_command(char *command) {
+    if (strcmp(command, "exit") == 0) {
+        exit_program();
+    } else if (strcmp(command, "clear") == 0) {
+        clear(); 
+    } else {
+        printf("Command not found: %s\n", command);
+    }
+}
+
+int main() {
+    char input[100];
+
+    while (1) {
+        printf("emu.root: ");
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = '\0';
+
+        execute_command(input);
+    }
+
+    return 0;
+}
