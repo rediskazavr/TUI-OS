39f236020dc0f055f2ac847650aba3ffd7cc9d7b
MatveySDK <matveywindows60@gmail.com>
2025-01-31 21:06:56 +0300

    Create func sys in kernelx

---
+++ src/kernelx/sys.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "sys.h"
+
+void sys() {
+    printf("Emulator version: alpha-dev 0.0.1\n");
+    printf("OS: Linux\n");
+    printf("GitHub Project: https://github.com/rediskazavr/TUI-OS\n");
+}
---
+++ src/kernelx/sys.h
@@ -0,0 +1,6 @@
+#ifndef SYS_H
+#define SYS_H
+
+void sys(void);
+
+#endif // SYS_H
